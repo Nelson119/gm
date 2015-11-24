7c24c8944d64f91f1d2bfcee0f1994f6d6924aa5
nelson <nelson119@outlook.com>
2015-11-24 17:49:43 +0800

    first commit

--- app/js/main.js
+++ app/js/main.js
@@ -3,23 +3,41 @@
 /*global  $, blueimp */
 
 $(function(){
+	//kv
 	if($('#kv').length){
 
-		blueimp.Gallery($('#kv .links'), {
-				container: '#blueimp-gallery-carousel',
-				clearSlides: false,
-				carousel: true,
-				indicatorOptions: {
-					// The tag name, Id, element or querySelector of the indicator container:
-					indicatorContainer: 'ol',
-					// The class for the active indicator:
-					activeIndicatorClass: 'active',
-					// Defines if the gallery indicators should display a thumbnail:
-					thumbnailIndicators: false,
+		// blueimp.Gallery($('#kv .links'), {
+		// 		container: '#blueimp-gallery-carousel',
+		// 		clearSlides: false,
+		// 		carousel: true,
+		// 		indicatorOptions: {
+		// 			// The tag name, Id, element or querySelector of the indicator container:
+		// 			indicatorContainer: 'ol',
+		// 			// The class for the active indicator:
+		// 			activeIndicatorClass: 'active',
+		// 			// Defines if the gallery indicators should display a thumbnail:
+		// 			thumbnailIndicators: false,
 
-					stretchImages:false
-				}
-			}
-		);
+		// 			stretchImages:false
+		// 		}
+		// 	}
+		// );
+		$('#kv ul').slick({
+		  dots: true,
+		  infinite: true,
+		  speed: 300,
+		  slidesToShow: 1,
+		  centerMode: true,
+		  variableWidth: true
+		});
 	}
+
+	//nav
+	if($('#menu').length){
+        $('#menu ul:eq(0)').superfish({
+            onShow: function(){
+            	console.log(this);
+            }
+        });
+    }
 });
