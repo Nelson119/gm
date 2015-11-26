058938021324f8b6fb763584383f6fa7ef6333a5
nelson <nelson119@outlook.com>
2015-11-26 17:56:08 +0800

    kv menu

--- app/js/main.js
+++ app/js/main.js
@@ -11,8 +11,14 @@ $(function(){
 		  speed: 300,
 		  slidesToShow: 1,
 		  centerMode: true,
-		  variableWidth: true
+		  variableWidth: true,
+		  autoplay: true,
+		  autoplaySpeed: 3000,
+		  easing: 'ease-out',
+		  pauseOnHover: true,
+		  speed: 750
 		});
+
 	}
 
 	//nav
@@ -22,40 +28,51 @@ $(function(){
 
 	        var kv = $('#kv');
 	        var menu = $('>.fit', header);
-			var tlheader = new TimelineMax({paused:true, onComplete:function(){}});
-
-			var originTop = header.find('.logo').css('padding-top').replace(/px/ig,'') * 1;
-			originTop = 666;
-			var originBottom = header.find('.logo').css('padding-bottom').replace(/px/ig,'') * 1;
+	        var logo = $('.logo', header);
+			var tlheader = new TimelineMax({paused:true, onStart:function(){
+	        	var scrollTop = $(window).scrollTop();
+				var h = menu.offset().top - scrollTop + 64;
+				TweenMax.set(kv,{height: h});
+			}});
+			var distance = 616;
 			
 
-			for(var i = 0;i <= originTop;i++){
-				var originScale = 2;
-				var smallScale = 0.5;
-				var scale = smallScale + (originTop - i) * (originScale - smallScale) / originTop;
-				var opacity = 1 - i / originTop;
-				var y = 300 / originTop * i;
+			for(var i = 0;i <= distance;i++){
+				var y = 0;
+				var opacity = 0;
+				if(i > 500){
+					y = ((i - 500) / 116) + 1;
+					opacity = (i - 500) / 116;
+				// var y = 300 / distance * i;
 
-
-				// tlheader.to(header.find('.logo'), 0.0000001, {paddingTop: (originTop - i )}, 'frame-' + i);
-				tlheader.to(kv, 0.000000001, {height: (originTop - i) }, 'frame-' + i);
-				// tlheader.to(kv, 1, {opacity: opacity }, 'frame-' + i);
-				// tlheader.to(header.find('.logo img'), 0.0000001, {scale: scale, y: -300 +y}, 'frame-' + i);
+				}
+				tlheader.to($('img', logo), 0.000000001, {y: y}, 'frame-' + i);
+				tlheader.to(logo, 0.000000001, {opacity: opacity}, 'frame-' + i);
 			}
 	        $(window).on('scroll', function(){
-	        	var scrollTop = $(window).scrollTop();
+	        	var o = this;
+			    // clearTimeout($.data(o, 'scrollTimer'));
+			    // $.data(o, 'scrollTimer', setTimeout(function() {
+		        	var scrollTop = $(window).scrollTop();
 
-				if(scrollTop < 666){
-					tlheader.tweenTo('frame-'+scrollTop);
-				}
-				if(scrollTop >= 666){
-					tlheader.tweenTo('frame-666');
-	        		menu.addClass('fixed');
-	        	}else{
-	        		menu.removeClass('fixed');
-	        	}
+					if(scrollTop <= 616){
+						tlheader.tweenTo('frame-'+scrollTop);
+						var h = menu.offset().top - scrollTop + 50;
+						TweenMax.set(kv,{height: h});
+					}
+					if(scrollTop >= 616){
+						tlheader.tweenTo('frame-616');
+		        		menu.addClass('fixed');
+		        		kv.addClass('pinned');
+						TweenMax.set(kv,{height: 50});
+		        	}else{
+		        		kv.removeClass('pinned');
+		        		menu.removeClass('fixed');
+		        	}
+			    // 	clearTimeout($.data(o, 'scrollTimer'));
+			    // }, 1));
 
-	        });
+	        }).trigger('scroll');
 			tlheader.tweenTo('frame-0');
 			window.tlheader = tlheader;
 		}($('header')));
